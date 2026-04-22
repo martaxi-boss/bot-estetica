368d37182b3e36c7d3309088d3f04d34c55745c0
martaxi-boss <martaxi.centro@gmail.com>
2026-04-22 02:27:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -398,6 +398,6 @@ def start_bot():
     bot_app.run_polling(stop_signals=None, drop_pending_updates=True)
 
 if __name__ == "__main__":
-    Thread(target=main, daemon=True).start()  # <- troca start_bot por main
+    Thread(target=start_bot, daemon=True).start()
     port = int(os.environ.get('PORT', 10000))
     app.run(host="0.0.0.0", port=port)
