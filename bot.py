8b86c783557c5e23616bdb48468da00c889bd3a4
martaxi-boss <martaxi.centro@gmail.com>
2026-04-22 12:01:20 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -400,7 +400,6 @@ async def setup():
     print("Webhook setado. Bot vivo 24h.", flush=True)
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(setup())
     port = int(os.environ.get('PORT', 10000))
     app.run(host="0.0.0.0", port=port)
